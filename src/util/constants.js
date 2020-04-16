4da370ad144de9317c75e7ddf17ba139bf405174
Alec N <alecng94@gmail.com>
2020-04-15 17:52:45 -0700

    remove unused constants

--- src/util/constants.js
+++ src/util/constants.js
@@ -8,34 +8,3 @@ const Constants = {
 }
 
 export default Constants;
-
-
-
-
-/**
- * Collection Definitions
- */
-export const COLLECTION_PHOTOGRAPHY = "photography";
-export const COLLECTION_TRIPREPORTS = "tripreports";
-
-export const VALID_COLLECTIONS = [
-  COLLECTION_PHOTOGRAPHY,
-  COLLECTION_TRIPREPORTS
-];
-
-export const collectionLabelMap = {
-  [COLLECTION_PHOTOGRAPHY]: 'Travel',
-  [COLLECTION_TRIPREPORTS]: 'Hiking'
-};
-
-export function getIndexRef(collection, firebase) {
-  let collectionMap = {};
-  collectionMap[COLLECTION_PHOTOGRAPHY] = firebase.photographyIndex();
-  collectionMap[COLLECTION_TRIPREPORTS] = firebase.tripreportIndex();
-  return collectionMap[collection];
-}
-
-/**
- * Collection specific fields for index
- */
-export const hikingIndexFields = ['area', 'region'];
