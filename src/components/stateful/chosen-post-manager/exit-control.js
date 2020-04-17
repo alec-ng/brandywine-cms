2ddbd3b5ea7345082d453bdddabd90cf8f339fe0
Alec N <alecng94@gmail.com>
2020-04-17 12:07:58 -0700

    refactored chosen post manager into separate components

---
+++ src/components/stateful/chosen-post-manager/exit-control.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { openModal, closeModal, close } from '../../../state/actions';
+
+/**
+ * Encapsulated exit functionality
+ */
+export default function PublishControl({ 
+  dispatch, 
+  save,
+  hasChanged
+}) {
+  function goBack() {
+    if (!hasChanged) {
+      dispatch(close());
+    } else {
+      const onSaveAndClose = () => { save(true) }
+      dispatch(openModal('unsavedChanges', {
+        onExit: closeCurrentPost,
+        onSave: onSaveAndClose  
+      }));
+    }
+  }
+
+  function closeCurrentPost() {
+    dispatch(close());
+    dispatch(closeModal());
+  }
+
+  return (
+    <BackBtn type="button" onClick={goBack}>
+      &#8592; Posts
+    </BackBtn>
+  );
+
+}
+
+const BackBtn = styled.button`
+  border: none;
+  position: absolute;
+  background: none;
+  padding: 0;
+  color: inherit;
+  top: 17px;
+`;
---
+++ src/components/stateful/chosen-post-manager/index.js
@@ -0,0 +1,121 @@
+import React, { useRef, useState, useEffect } from "react";
+import { connect } from 'react-redux';
+import { withFirebase } from '../../hoc/firebase';
+import usePrevious from '../../../hooks/usePrevious';
+import Slug from '../../../modules/slug';
+
+import { selectPendingStatus } from '../../../state/selectors';
+import { openModal, updateMetadata, close } from '../../../state/actions';
+import { savePost, SAVE_CURRENT_POST } from '../../../state/actions/async-actions';
+
+import MetadataForm from "../../stateless/global/forms/metadata-form";
+import Spinner from "../../stateless/generic/spinner";
+import ExitControl from './exit-control';
+import PublishControl from './publish-control';
+import DeleteControl from './delete-control';
+import { StretchBtn } from '../../stateless/generic/util'
+
+/**
+ * Component manager for toolbar view when a post is selected
+ */
+function ChosenPostManager(props) {
+  const { data, chosenPost, savePending, dispatch, firebase } = props;
+
+  // Track if any change has been made
+  const [hasChanged, setHasChanged] = useState(false);
+  const prevChosenPost = usePrevious(chosenPost);
+  const disableSave = hasChanged ? {} : { disabled: true };
+
+  useEffect(() => {
+    if (prevChosenPost && !hasChanged) {
+      setHasChanged(true);
+    }
+  }, [chosenPost, prevChosenPost, hasChanged])
+
+  
+  // updates value in store after validation, and optionally "closes" afterwawrd
+  function save(closePostAfterCompletion = false) {
+    if (!validate(chosenPost.post.isPublished)) {
+      return;
+    }
+    dispatch(
+      savePost(firebase, chosenPost)
+    ).then(() => {
+      if (closePostAfterCompletion) {
+        dispatch(close());
+      }
+    }).catch(err => { console.error(err) });
+  }
+
+
+  // Chosen post validation using 4 apis: HTML form, Slug, CMSPost, PostMetadata
+  const formRef = useRef(null);
+
+  function validate(willPublish) {
+    if (!formRef.current.reportValidity()) {
+      return false;
+    }
+    
+    // Basic empty check before specialized validation logic
+    let errs = chosenPost.post.validateBaseProps();
+    if (!errs.length) {
+      const isValidSlug = Slug.validateUniqueAndChanged(chosenPost, data);
+      if (!isValidSlug) {
+        errs.push('The slug already exists for the given date/title.')
+      }
+      if (willPublish) {
+        errs = errs.concat(chosenPost.publishValidation());
+      }
+    }
+
+    if (errs.length) {
+      dispatch(openModal('validationError', {errors: errs}));
+      return false;
+    }
+    return true;
+  }
+
+  
+  // Sync chosen post in store with changes to form
+  function onMetadataChange(e) {
+    dispatch(updateMetadata(e.target.name, e.target.value));
+  }
+
+  return (
+    <>
+      <ExitControl {...props} save={save} hasChanged={hasChanged} />
+      
+      <form ref={formRef}>
+        <fieldset disabled={savePending}>
+          <MetadataForm
+            onInputChange={onMetadataChange}
+            cmsPost={chosenPost}
+          />
+          <div className="my-3">
+            <StretchBtn
+              type="button"
+              className="my-2 btn btn-success"
+              onClick={ () => { save(false) } }
+              {...disableSave}
+            >
+              {savePending && <Spinner />}
+              Save
+            </StretchBtn>
+
+            <PublishControl {...props} validate={validate} />
+            <DeleteControl {...props} />
+          </div>
+        </fieldset>
+      </form>
+    </>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    chosenPost: state.chosenPost,
+    data: state.data,
+    savePending: selectPendingStatus(state, SAVE_CURRENT_POST)
+  }
+}
+export default connect(mapStateToProps)(withFirebase(ChosenPostManager));
